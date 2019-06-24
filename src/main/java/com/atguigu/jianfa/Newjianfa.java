47afa58051f4314901ef402e7262af474327992e
linghuchong <a184019544@qq.com>
2019-06-24 19:12:37 +0800

    new jianfa

---
+++ src/main/java/com/atguigu/jianfa/Newjianfa.java
@@ -0,0 +1,11 @@
+package com.atguigu.jianfa;
+
+/**
+ * @author shkstart
+ * @ date 2019-06-24 19:11
+ */
+public class Newjianfa {
+    public static void main(String[] args) {
+        System.out.println("new jianfa");
+    }
+}
