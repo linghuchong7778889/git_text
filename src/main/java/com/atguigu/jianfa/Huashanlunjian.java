7c40fd864fb68e1a129841b1bc64b42d66b38324
linghuchong <a184019544@qq.com>
2019-06-24 18:27:21 +0800

    huashanjianfa

--- src/main/java/com/atguigu/jianfa/Huashanlunjian.java
+++ src/main/java/com/atguigu/jianfa/Huashanlunjian.java
@@ -2,10 +2,10 @@ package com.atguigu.jianfa;
 
 /**
  * @author shkstart
- * @ date 2019-06-24 18:14
+ * @ date 2019-06-24 18:23
  */
-public class Dugujiujian {
+public class Huashanlunjian {
     public static void main(String[] args) {
-        System.out.println("hello,dugujiujiansss");
+        System.out.println("Huashanlunjian");
     }
 }
